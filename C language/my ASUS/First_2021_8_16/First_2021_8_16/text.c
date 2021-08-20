94116ed45d6eef57523af4eef7f11a9ab39bc700
ljq <lijianqiang2001@yeah.net>
2021-08-21 00:18:19 +0800

    练习

--- C language/my ASUS/First_2021_8_16/First_2021_8_16/text.c
+++ C language/my ASUS/First_2021_8_16/First_2021_8_16/text.c
@@ -1,22 +1,115 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <stdio.h>
-//递归求字符串长度，不适用临时变量
-int my_strlen(char* str)
-{
-	if (*str != '\0')
-		return 1 + my_strlen(str + 1);
-	else
-		return 0;
-}
 
+//int Fib(int n)
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n > 2)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+//int main()
+//{
+//	int n = 0;
+//	int srt = 0;
+//	scanf("%d", &n);
+//	srt = Fib(n);
+//	printf("%d\n", srt);
+//	return 0;
+//}
+
+//int count = 0;
+//int Fib(int n)
+//{
+//	if (n == 3)
+//	{
+//		count++;
+//	}
+//	if (n <= 2)
+//		return 1;
+//	else
+//		return Fib(n - 1) + Fib(n - 2);
+//}
+//int main()
+//{
+//	int n = 0;
+//	int srt = 0;
+//	scanf("%d", &n);
+//	srt = Fib(n);
+//	printf("%d\n", srt);
+//	printf("%d\n", count);
+//	return 0;
+//}
+
+//递归求 n 的阶乘
+//int Fac2(int n)
+//{
+//	if (n <= 1)
+//	{
+//		return 1;
+//	}
+//	else
+//		return n * Fac2(n - 1);
+//}
+//int main()
+//{
+//	int i = 0;
+//	int ret = 0;
+//	scanf("%d", &i);
+//	ret = Fac2(i);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+//循环求 n 的阶乘
+int Fac1(int num)
+{
+	int i = 0;
+	int ret = 1;
+	for (i = 1; i <= num; i++)
+	{
+		ret *= i;
+	}
+	return ret;
+}
 int main()
 {
-	char arr[] = "bit";
-	int len = my_strlen(arr);
-	printf("len = %d\n", len);
+	int n = 10;
+	int i = 0;
+	while (n != 0)
+	{
+		int ret = 0;
+		scanf("%d", &i);
+		ret = Fac1(i);
+		printf("%d\n", ret);
+		n--;
+	}
 	return 0;
 }
+
+//递归求字符串长度，不适用临时变量
+//int my_strlen(char* str)
+//{
+//	if (*str != '\0')
+//		return 1 + my_strlen(str + 1);
+//	else
+//		return 0;
+//}
+//
+//int main()
+//{
+//	char arr[] = "bit";
+//	int len = my_strlen(arr);
+//	printf("len = %d\n", len);
+//	return 0;
+//}
 
 //函数求字符串长度
 //int my_strlen(char* str)
